bdb4af29253b46d9c57d2fb6b3ebe456481656ff
Angela N Salvador <angela.s.salvador@gmail.com>
2021-05-07 08:35:26 -0300

    Script in C for 1.Hello task

--- 0x00-hello_world/1-puts.c
+++ 0x00-hello_world/1-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
-*Using Puts
-*Prints: Programming is like building a multilingual puzzle,
-*followed by a new line.
+* main - Print a phrase
+* @parameterx: No parameters
 *
+* Return: Prints Programming is like building a multilingual puzzle
 */
+
 int main(void)
+	
 {
-	puts ("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
