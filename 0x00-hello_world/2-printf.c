a44644547dfe2f5790b92e53f60d33ecc93774c3
Angela N Salvador <angela.s.salvador@gmail.com>
2021-05-07 09:25:25 -0300

    Script in C for 2.Hello Printf task

--- 0x00-hello_world/2-printf.c
+++ 0x00-hello_world/2-printf.c
@@ -3,6 +3,7 @@
 /** main - print something
 * Return: Prints with proper grammar, but the outcome is a piece of art,
 */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
