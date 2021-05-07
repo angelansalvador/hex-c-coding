5e2fbd370ebdf1a7c41a6a6bfbeb45ba3de90d3f
Angela N Salvador <angela.s.salvador@gmail.com>
2021-05-07 08:36:43 -0300

    Script in C for 1.Hello task

--- 0x00-hello_world/1-puts.c
+++ 0x00-hello_world/1-puts.c
@@ -2,13 +2,11 @@
 
 /**
 * main - Print a phrase
-* @parameterx: No parameters
-*
+* 
 * Return: Prints Programming is like building a multilingual puzzle
 */
 
 int main(void)
-	
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
