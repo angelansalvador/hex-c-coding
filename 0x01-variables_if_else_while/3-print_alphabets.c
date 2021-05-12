5c0d15d448a9ff0e9e18e6e0bf7dfc486f38cd2c
Angela N Salvador <angela.s.salvador@gmail.com>
2021-05-12 13:33:08 -0300

    Script in c to print alphabet lower and high case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * * main - printing the lowercase alfabet
+ * * Return: Prints abcdefghjk...z
+ * */
+int main(void)
+{
+	int ascii_code;
+
+	for (ascii_code = 97; ascii_code<123; ascii_code++)
+	{
+	putchar(ascii_code);
+	}
+
+	for (ascii_code = 65; ascii_code<91; ascii_code++)
+	{
+	putchar(ascii_code);
+	}
+	
+	putchar('\n');
+	
+	return (0);
+}
