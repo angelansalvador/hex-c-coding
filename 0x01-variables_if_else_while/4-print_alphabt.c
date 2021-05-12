c1c78286ca587fb8c681e87ffdb478e23f31dca1
Angela N Salvador <angela.s.salvador@gmail.com>
2021-05-12 14:13:15 -0300

    Script in c to print alphabet without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - printing the lowercase alfabet
+* Return: Prints abcdefghjk...z
+*/
+int main(void)
+{
+	int ascii_code;
+
+	for (ascii_code = 97; ascii_code < 123; ascii_code++)
+	{
+		if (ascii_code == 101 or ascii_code == 113)
+		{
+		}
+		else
+		{
+		putchar(ascii_code);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
