add695a1c3cfc13cbd1012a10b03e8c1883a33b3
Angela N Salvador <angela.s.salvador@gmail.com>
2021-05-12 14:45:37 -0300

    Script in C that prints number 0 to 9 through ascii

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - printing the number 0 to 9 
+* main - printing the number 0 to 9
 * Return: Prints 0 to 9
 */
 int main(void)
 {
 	int ascii_code;
-			
+	
 	for (ascii_code = 48; ascii_code < 58; ascii_code++)
 	{
 	putchar(ascii_code);
