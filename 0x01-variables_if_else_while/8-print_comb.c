bc93b2994b2fb24bef520990ff9cf1120b039bcd
Angela N Salvador <angela.s.salvador@gmail.com>
2021-05-13 13:21:51 -0300

    Script in C prints 0 to 9 with commma and space

---
+++ 0x01-variables_if_else_while/8-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - printing number with , and space
+* Return: Prints 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+*/
+int main(void)
+{
+	int ascii_code;
+
+	for (ascii_code = 48; ascii_code < 58; ascii_code++)
+	{
+	putchar(ascii_code);
+	putchar(','); 
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
