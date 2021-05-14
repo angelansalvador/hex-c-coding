c557d68a35ec35fdbdeb151bbb35fd8c8718fedb
Angela N Salvador <angela.s.salvador@gmail.com>
2021-05-14 16:27:31 -0300

    Script in C prints 0 to 9 with commma and space

--- 0x01-variables_if_else_while/8-print_comb.c
+++ 0x01-variables_if_else_while/8-print_comb.c
@@ -7,12 +7,18 @@
 int main(void)
 {
 	int ascii_code;
-
 	for (ascii_code = 48; ascii_code < 58; ascii_code++)
 	{
 	putchar(ascii_code);
-	putchar(','); 
-	putchar(' ');
+
+		if (ascii_code == 57)
+		{
+		}
+		else
+		{
+		putchar(',');
+		putchar(' ');
+		}	
 	}
 	putchar('\n');
 	return (0);
