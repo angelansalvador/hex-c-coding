6fa412fd92411a9abe477405e573b5b6d00c8a5e
Angela N Salvador <angela.s.salvador@gmail.com>
2021-05-18 09:51:38 -0300

    Script with putchar that prints 'Holberton'

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - printing the lowercase alfabet
+* Return: Always 0
+*/
+int main(void)
+{
+	putchar('H');
+	putchar('o');
+	putchar('l');
+	putchar('b');
+	putchar('e');
+	putchar('r');
+	putchar('t');
+	putchar('o');
+	putchar('n');
+	putchar('\n');
+	return (0);
+}
