3e4bee8d2c85dd8e80c30673779084e688444850
Angela N Salvador <angela.s.salvador@gmail.com>
2021-05-19 09:15:54 -0300

    Task 0._putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include "holberton.h"
 
 /**
-* main - printing the lowercase alfabet
+* main - printing Holberton
 * Return: Always 0
 */
 int main(void)
 {
-	putchar('H');
-	putchar('o');
-	putchar('l');
-	putchar('b');
-	putchar('e');
-	putchar('r');
-	putchar('t');
-	putchar('o');
-	putchar('n');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	putchar('\n');
 	return (0);
 }
