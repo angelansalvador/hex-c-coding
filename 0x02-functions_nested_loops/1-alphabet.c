4049fda264426921fb3753f0a35a72871d9a9aca
Angela N Salvador <angela.s.salvador@gmail.com>
2021-05-19 14:25:45 -0300

    Task to write alphabet with function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 
 /**
-* main - printing the lowercase alfabet
+* print_alphabet - printing the lowercase alfabet
 * Return: Prints abcdefghjk...z
 */
 void print_alphabet(void)
 {
 	int ascii_code;
+
 	for (ascii_code = 97; ascii_code < 123; ascii_code++)
 	{
 	_putchar(ascii_code);
