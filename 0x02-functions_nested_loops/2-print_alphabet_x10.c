2829b8d32d2c130619fe8f0c3c9188ef8e5a7f85
Angela N Salvador <angela.s.salvador@gmail.com>
2021-06-17 15:44:26 -0300

    function that prints 10 times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,14 @@
 * print_alphabet - printing 10x alphabet
 * Return: Prints abcdefghjk...z 10X
 */
-void print_alphabet_x10(void)
+void print_alphabet()
 {
 	int ascii_code;
-	int i = 0;
-
-	for (i=0; i<10; i++)
+	for (int i=0; i<10; i++)
 	{
 		for (ascii_code = 97; ascii_code < 123; ascii_code++)
-		{	
-       			_putchar(ascii_code);
+		{
+		_putchar(ascii_code);
 		}
 		_putchar('\n');
 	}
