a6b80a848238d347eeb6f40c59ed56d3772eab29
Angela N Salvador <angela.s.salvador@gmail.com>
2021-06-17 15:56:09 -0300

    function that prints 10 times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
 
 /**
-* print_alphabet - printing 10x alphabet
+* print_alphabet_x10 - printing 10x alphabet
 * Return: Prints abcdefghjk...z 10X
 */
-void print_alphabet_x10()
+void print_alphabet_x10(void)
 {
 	int ascii_code;
 	int i = 0;
-	do
-	{
+
+	do{
 		for (ascii_code = 97; ascii_code < 123; ascii_code++)
 		{
 		_putchar(ascii_code);
 		}
 		_putchar('\n');
 		i++;
-	} while (i<10);
+	} while (i < 10);
 }
 
