d0144e55220a4920265425e99b970eeab78ebd3e
Angela N Salvador <angela.s.salvador@gmail.com>
2021-09-22 09:33:02 -0300

    computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* _abs - put int in absolute
+* @i: int
+* Return: absolute int
+*/
+int _abs(int i)
+{
+
+	if (i >= 0)
+	{
+	return (i);
+	}
+	else
+	{
+	return (i * -1);
+	}
+}
